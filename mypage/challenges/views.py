f1dee10c68559dadff1026596d80aa8745c8d2d0
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-10-24 18:18:26 +0100

    new file:   .vscode/launch.json

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.template.loader import render_to_string 
 
 # Create your views here.
 
@@ -39,9 +40,13 @@ def monthly_challenge_by_number(request, month):
 
 def monthly_challenge(request, month):
     try:
+        
         challenges_text = monthly_challenges[month]
-        response_data = f'<h1>{challenges_text}</h1>'
-        return HttpResponse(response_data)
+        challenge_head = month.capitalize() 
+        return render(request, "challenges/challenge.html", {"text": challenges_text, "head":challenge_head})
+        # # response_data = f'<h1>{challenges_text}</h1>'
+        # response_data = render_to_string()
+        # return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("not found at all ")
 
